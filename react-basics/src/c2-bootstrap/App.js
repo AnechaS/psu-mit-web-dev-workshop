27d5e90a655242ad84011e161650c8fde3eaa40e
Anecha Sun <Anachitheu.k@gmail.com>
2021-02-26 06:45:47 +0700

    Update c2

--- react-basics/src/c2-bootstrap/App.js
+++ react-basics/src/c2-bootstrap/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import { Card } from "react-bootstrap";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Card>
-          <Card.Body>This is some text within a card body.</Card.Body>
-        </Card>
+      <div className="container pt-3">
+        <div>
+          <h3>Alert</h3>
+          <div className="alert alert-primary" role="alert">
+            A simple primary alert—check it out!
+          </div>
+        </div>
+
+        <div className="mt-3">
+          <h3>Button</h3>
+          <button className="btn btn-primary">Primary</button>
+        </div>
+
+        <div className="mt-3">
+          <h3>Card</h3>
+          <div className="card">
+            <div className="card-body">
+              This is some text within a card body.
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
