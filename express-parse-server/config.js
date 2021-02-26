243ef05ca24a75e08f81d4582d4311652f7d2910
Anecha Sun <Anachitheu.k@gmail.com>
2021-02-26 18:25:53 +0700

    Update gitignore

---
+++ express-parse-server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    appId: 'qwertyuio',
+    databaseURI: 'mongodb://localhost:27017/todo',
+    masterKey: 'asdfghjk',
+    restAPIKey: 'zxcvbnm',
+    serverURL: 'http://localhost:1337/parse',
+    port: 1337
+};
